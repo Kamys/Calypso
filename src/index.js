9a294a1b8a50bf61e81ed30bcaf61d9d1ec49699
Nikita <nikita.kamyshenko@mail.ru>
2018-02-14 09:53:33 +0300

    Fix button bug

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'materialize-css/dist/css/materialize.min.css'
-import 'materialize-css/dist/js/materialize.min'
+import $ from 'jquery/dist/jquery'
 import './index.css'
+window.jQuery = $;
+window.$ = $;
+
+require('materialize-css/dist/css/materialize.min.css');
+require('materialize-css/dist/js/materialize');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
